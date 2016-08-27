ab30b7d7f7696bde24679a7935d14cb2d7623edc
nickp10 <nickp10@hotmail.com>
2016-08-27 17:55:42 -0600

    Setting up the NumberFire parser for each of the sports/sites combos

--- src/retrievers/numberFire.ts
+++ src/retrievers/numberFire.ts
@@ -1,44 +1,93 @@
 /// <reference path="../../typings/index.d.ts" />
 
 import { IDataRetriever, ISiteDataRetriever, IPlayer } from "../interfaces";
-import * as cookie from "cookie";
 import * as http from "http";
 import * as https from "https";
 import * as Promise from "promise";
+import * as setCookieParser from "set-cookie-parser";
 
 interface IIncomingMessage extends http.IncomingMessage {
 	body?: string;
 }
 
 export default class NumberFire implements IDataRetriever {
+	static mlbSetSiteURL = "/mlb/daily-fantasy/set-dfs-site";
+	static nbaSetSiteURL = "/nba/daily-fantasy/set-dfs-site";
+	static nflSetSiteURL = "/nfl/daily-fantasy/set-dfs-site";
+	static nhlSetSiteURL = "/nhl/daily-fantasy/set-dfs-site";
+
+	static mlbDataSiteURLs = [
+		"/mlb/daily-fantasy/daily-baseball-projections/pitchers",
+		"/mlb/daily-fantasy/daily-baseball-projections/batters"
+	];
+	static nbaDataSiteURLs = [
+		"",
+		""
+	];
+	static nflDataSiteURLs = [
+		"",
+		""
+	];
+	static nhlDataSiteURLs = [
+		"",
+		""
+	];
+
+	static fanDuelID = "3";
+	static draftKingsID = "4";
+	static yahooID = "13";
+
 	fanDuel = {
-		mlb: this.getData.bind(this),
-		nba: this.getData.bind(this),
-		nfl: this.getData.bind(this),
-		nhl: this.getData.bind(this)
+		mlb: () => this.getData(NumberFire.mlbSetSiteURL, NumberFire.fanDuelID, NumberFire.mlbDataSiteURLs),
+		nba: () => this.getData(NumberFire.nbaSetSiteURL, NumberFire.fanDuelID, NumberFire.nbaDataSiteURLs),
+		nfl: () => this.getData(NumberFire.nflSetSiteURL, NumberFire.fanDuelID, NumberFire.nflDataSiteURLs),
+		nhl: () => this.getData(NumberFire.nhlSetSiteURL, NumberFire.fanDuelID, NumberFire.nhlDataSiteURLs)
 	};
 
-	getData(): Promise.IThenable<IPlayer[]> {
+	draftKings = {
+		mlb: () => this.getData(NumberFire.mlbSetSiteURL, NumberFire.draftKingsID, NumberFire.mlbDataSiteURLs),
+		nba: () => this.getData(NumberFire.nbaSetSiteURL, NumberFire.draftKingsID, NumberFire.nbaDataSiteURLs),
+		nfl: () => this.getData(NumberFire.nflSetSiteURL, NumberFire.draftKingsID, NumberFire.nflDataSiteURLs),
+		nhl: () => this.getData(NumberFire.nhlSetSiteURL, NumberFire.draftKingsID, NumberFire.nhlDataSiteURLs)
+	};
+
+	yahoo = {
+		mlb: () => this.getData(NumberFire.mlbSetSiteURL, NumberFire.yahooID, NumberFire.mlbDataSiteURLs),
+		nba: () => this.getData(NumberFire.nbaSetSiteURL, NumberFire.yahooID, NumberFire.nbaDataSiteURLs),
+		nfl: () => this.getData(NumberFire.nflSetSiteURL, NumberFire.yahooID, NumberFire.nflDataSiteURLs),
+		nhl: () => this.getData(NumberFire.nhlSetSiteURL, NumberFire.yahooID, NumberFire.nhlDataSiteURLs)
+	};
+
+	getData(setSiteURL: string, siteID: string, dataSiteURLs: string[]): Promise.IThenable<IPlayer[]> {
 		return this.sendHttpsRequest({
 			hostname: "www.numberfire.com",
-			path: "/mlb/daily-fantasy/daily-baseball-projections/pitchers",
-			method: "GET"
-		}).then((resp) => {
-			resp.rawHeaders.forEach(element => {
-				console.log(element);
+			path: setSiteURL,
+			method: "POST"
+		}, `site=${siteID}`).then((setSiteResp) => {
+			const setCookies = setCookieParser(setSiteResp);
+			const cookieHeader = setCookies.map(c => `${c.name}=${c.value}`);
+			return this.sendHttpsRequest({
+				hostname: "www.numberfire.com",
+				path: dataSiteURLs[0],
+				method: "GET",
+				headers: {
+					Cookie: cookieHeader
+				}
+			}).then((dataResp) => {
+				return undefined;
 			});
-			/*resp.headers["set-cookie"].forEach((cookieHeader) => {
-				var c = cookie.parse(cookieHeader);
-				var d: CookieParseOptions;
-			});
-			console.log(resp.body);*/
-			return undefined;
 		});
 	}
 
-	sendHttpsRequest(request: https.RequestOptions): Promise.IThenable<IIncomingMessage> {
+	sendHttpsRequest(request: https.RequestOptions, data?: string): Promise.IThenable<IIncomingMessage> {
 		return new Promise<IIncomingMessage>((resolve, reject) => {
-			https.request(request, (resp: IIncomingMessage) => {
+			const headers = request.headers || { };
+			request.headers = headers;
+			if (data) {
+				headers["Content-Type"] = "application/x-www-form-urlencoded";
+				headers["Content-Length"] = data.length;
+			}
+			const req = https.request(request, (resp: IIncomingMessage) => {
 				let body = "";
 				resp.on("data", (data) => {
 					body += data;
@@ -49,7 +98,11 @@ export default class NumberFire implements IDataRetriever {
 				});
 			}).on("error", (error: Error) => {
 				reject(error.message);
-			}).end();
+			});
+			if (data) {
+				req.write(data);
+			}
+			req.end();
 		});
 	}
 }
