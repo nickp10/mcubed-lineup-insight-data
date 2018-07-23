6bcd85031f9ce69ee03455a73a57be84ae560402
nickp10 <nickp10@hotmail.com>
2018-07-23 14:24:52 -0600

    Fix the DFSR data retriever based on the new CSV formats

--- src/retrievers/dfsr.spec.ts
+++ src/retrievers/dfsr.spec.ts
@@ -8,58 +8,137 @@ describe("DFSR", () => {
     describe("#parsePlayers()", () => {
         it("should parse the projection stats for an MLB DraftKings contest", () => {
             // Arrange
+            const csvOptions = (<any>DFSR).draftKingsMLBCSVOptions;
             const target = new DFSR();
-            const data = fs.readFileSync("spec-content/dfsrMLBDraftKings.csv", "utf-8");
+            const data = fs.readFileSync("spec-content/dfsrMLB.csv", "utf-8");
             const playerFactory = new PlayerFactory(Sport.MLB);
 
             // Act
-            const players = target.parsePlayers(playerFactory, data, 1, 10, 2, 3);
+            const players = target.parsePlayers(playerFactory, data, csvOptions);
 
             // Assert
-            specUtils.assertContainsPlayer(players, { name: "Josh Donaldson", team: "TOR", salary: 5400, stats: [ { source: "DailyFantasySportsRankings", projectedPoints: 9.811496235 }] });
-            specUtils.assertContainsPlayer(players, { name: "David Ortiz", team: "BOS", salary: 5200, stats: [ { source: "DailyFantasySportsRankings", projectedPoints: 10.37103443 }] });
+            specUtils.assertContainsPlayer(players, { name: "Mookie Betts", team: "BOS", salary: 6200, stats: [ { source: "DailyFantasySportsRankings", projectedPoints: 10.50953059, projectedFloor: 5.465665619, projectedCeiling: 15.55339556 }] });
+            specUtils.assertContainsPlayer(players, { name: "Rhys Hoskins", team: "PHI", salary: 3900, stats: [ { source: "DailyFantasySportsRankings", projectedPoints: 8.241344116, projectedFloor: 4.20486106, projectedCeiling: 12.27782717 }] });
         });
 
         it("should parse the projection stats for an MLB FanDuel contest", () => {
             // Arrange
+            const csvOptions = (<any>DFSR).fanDuelMLBCSVOptions;
             const target = new DFSR();
-            const data = fs.readFileSync("spec-content/dfsrMLBFanDuel.csv", "utf-8");
+            const data = fs.readFileSync("spec-content/dfsrMLB.csv", "utf-8");
             const playerFactory = new PlayerFactory(Sport.MLB);
 
             // Act
-            const players = target.parsePlayers(playerFactory, data, 1, 10, 2, 3);
+            const players = target.parsePlayers(playerFactory, data, csvOptions);
 
             // Assert
-            specUtils.assertContainsPlayer(players, { name: "Josh Donaldson", team: "TOR", salary: 3900, stats: [ { source: "DailyFantasySportsRankings", projectedPoints: 12.91184611 }] });
-            specUtils.assertContainsPlayer(players, { name: "David Ortiz", team: "BOS", salary: 4300, stats: [ { source: "DailyFantasySportsRankings", projectedPoints: 13.78429798 }] });
+            specUtils.assertContainsPlayer(players, { name: "Mookie Betts", team: "BOS", salary: 5000, stats: [ { source: "DailyFantasySportsRankings", projectedPoints: 13.90805479, projectedFloor: 8.808734045, projectedCeiling: 19.00737553 }] });
+            specUtils.assertContainsPlayer(players, { name: "Rhys Hoskins", team: "PHI", salary: 3800, stats: [ { source: "DailyFantasySportsRankings", projectedPoints: 11.05682593, projectedFloor: 6.794435271, projectedCeiling: 15.31921659 }] });
+        });
+
+        it("should parse the projection stats for an MLB Yahoo contest", () => {
+            // Arrange
+            const csvOptions = (<any>DFSR).yahooMLBCSVOptions;
+            const target = new DFSR();
+            const data = fs.readFileSync("spec-content/dfsrMLB.csv", "utf-8");
+            const playerFactory = new PlayerFactory(Sport.MLB);
+
+            // Act
+            const players = target.parsePlayers(playerFactory, data, csvOptions);
+
+            // Assert
+            specUtils.assertContainsPlayer(players, { name: "Mookie Betts", team: "BOS", salary: undefined, stats: [ { source: "DailyFantasySportsRankings", projectedPoints: undefined }] });
+            specUtils.assertContainsPlayer(players, { name: "Rhys Hoskins", team: "PHI", salary: undefined, stats: [ { source: "DailyFantasySportsRankings", projectedPoints: undefined }] });
+        });
+
+        it("should parse the projection stats for an NBA DraftKings contest", () => {
+            // Arrange
+            const csvOptions = (<any>DFSR).draftKingsNBACSVOptions;
+            const target = new DFSR();
+            const data = fs.readFileSync("spec-content/dfsrNBA.csv", "utf-8");
+            const playerFactory = new PlayerFactory(Sport.NBA);
+
+            // Act
+            const players = target.parsePlayers(playerFactory, data, csvOptions);
+
+            // Assert
+            specUtils.assertContainsPlayer(players, { name: "LeBron James", team: "CLE", salary: 20000, stats: [ { source: "DailyFantasySportsRankings", projectedPoints: 73.94516769 }] });
+            specUtils.assertContainsPlayer(players, { name: "Al Horford", team: "BOS", salary: 11700, stats: [ { source: "DailyFantasySportsRankings", projectedPoints: 41.86153742 }] });
+        });
+
+        it("should parse the projection stats for an NBA FanDuel contest", () => {
+            // Arrange
+            const csvOptions = (<any>DFSR).fanDuelNBACSVOptions;
+            const target = new DFSR();
+            const data = fs.readFileSync("spec-content/dfsrNBA.csv", "utf-8");
+            const playerFactory = new PlayerFactory(Sport.NBA);
+
+            // Act
+            const players = target.parsePlayers(playerFactory, data, csvOptions);
+
+            // Assert
+            specUtils.assertContainsPlayer(players, { name: "LeBron James", team: "CLE", salary: 13700, stats: [ { source: "DailyFantasySportsRankings", projectedPoints: 71.13370437 }] });
+            specUtils.assertContainsPlayer(players, { name: "Al Horford", team: "BOS", salary: 7400, stats: [ { source: "DailyFantasySportsRankings", projectedPoints: 41.37531445 }] });
         });
 
         it("should parse the projection stats for an NFL DraftKings contest", () => {
             // Arrange
+            const csvOptions = (<any>DFSR).draftKingsNFLCSVOptions;
             const target = new DFSR();
             const data = fs.readFileSync("spec-content/dfsrNFLDraftKings.csv", "utf-8");
             const playerFactory = new PlayerFactory(Sport.NFL);
 
             // Act
-            const players = target.parsePlayers(playerFactory, data, 1, 2, 4, 6);
+            const players = target.parsePlayers(playerFactory, data, csvOptions);
 
             // Assert
-            specUtils.assertContainsPlayer(players, { name: "Julio Jones", team: "ATL", salary: 9600, stats: [ { source: "DailyFantasySportsRankings", projectedPoints: 20.82037324 }] });
-            specUtils.assertContainsPlayer(players, { name: "Aaron Rodgers", team: "GB", salary: 7700, stats: [ { source: "DailyFantasySportsRankings", projectedPoints: 22.44357271 }] });
+            specUtils.assertContainsPlayer(players, { name: "Tom Brady", team: "NE", salary: 7700, stats: [ { source: "DailyFantasySportsRankings", projectedPoints: 17.86729699 }] });
+            specUtils.assertContainsPlayer(players, { name: "Zach Ertz", team: "PHI", salary: 5300, stats: [ { source: "DailyFantasySportsRankings", projectedPoints: 12.56613019 }] });
         });
 
         it("should parse the projection stats for an NFL FanDuel contest", () => {
             // Arrange
+            const csvOptions = (<any>DFSR).fanDuelNFLCSVOptions;
             const target = new DFSR();
             const data = fs.readFileSync("spec-content/dfsrNFLFanDuel.csv", "utf-8");
             const playerFactory = new PlayerFactory(Sport.NFL);
 
             // Act
-            const players = target.parsePlayers(playerFactory, data, 1, 2, 4, 6);
+            const players = target.parsePlayers(playerFactory, data, csvOptions);
 
             // Assert
-            specUtils.assertContainsPlayer(players, { name: "Julio Jones", team: "ATL", salary: 8900, stats: [ { source: "DailyFantasySportsRankings", projectedPoints: 17.25377929 }] });
-            specUtils.assertContainsPlayer(players, { name: "Aaron Rodgers", team: "GB", salary: 9100, stats: [ { source: "DailyFantasySportsRankings", projectedPoints: 21.84755108 }] });
+            specUtils.assertContainsPlayer(players, { name: "Tom Brady", team: "NE", salary: 9000, stats: [ { source: "DailyFantasySportsRankings", projectedPoints: 17.41886223 }] });
+            specUtils.assertContainsPlayer(players, { name: "Zach Ertz", team: "PHI", salary: 7000, stats: [ { source: "DailyFantasySportsRankings", projectedPoints: 10.21689655 }] });
+        });
+
+        it("should parse the projection stats for an NHL DraftKings contest", () => {
+            // Arrange
+            const csvOptions = (<any>DFSR).draftKingsNHLCSVOptions;
+            const target = new DFSR();
+            const data = fs.readFileSync("spec-content/dfsrNHL.csv", "utf-8");
+            const playerFactory = new PlayerFactory(Sport.NHL);
+
+            // Act
+            const players = target.parsePlayers(playerFactory, data, csvOptions);
+
+            // Assert
+            specUtils.assertContainsPlayer(players, { name: "Alex Ovechkin", team: "WAS", salary: 7600, stats: [ { source: "DailyFantasySportsRankings", projectedPoints: 5.115410248 }] });
+            specUtils.assertContainsPlayer(players, { name: "Patric Hornqvist", team: "PIT", salary: 6100, stats: [ { source: "DailyFantasySportsRankings", projectedPoints: 2.747319079 }] });
+        });
+
+        it("should parse the projection stats for an NHL FanDuel contest", () => {
+            // Arrange
+            const csvOptions = (<any>DFSR).fanDuelNHLCSVOptions;
+            const target = new DFSR();
+            const data = fs.readFileSync("spec-content/dfsrNHL.csv", "utf-8");
+            const playerFactory = new PlayerFactory(Sport.NHL);
+
+            // Act
+            const players = target.parsePlayers(playerFactory, data, csvOptions);
+
+            // Assert
+            specUtils.assertContainsPlayer(players, { name: "Alex Ovechkin", team: "WAS", salary: 9000, stats: [ { source: "DailyFantasySportsRankings", projectedPoints: 17.53975396 }] });
+            specUtils.assertContainsPlayer(players, { name: "Patric Hornqvist", team: "PIT", salary: 6700, stats: [ { source: "DailyFantasySportsRankings", projectedPoints: 9.074171053 }] });
         });
     });
 });
--- src/retrievers/dfsr.ts
+++ src/retrievers/dfsr.ts
@@ -2,57 +2,112 @@ import { IPlayer, IPlayerInsightRetriever, IPlayerStats, ContestType, Sport } fr
 import PlayerFactory from "../playerFactory";
 import utils from "../utils";
 
-interface IIndexMapping {
+interface ICSVOptions {
+    isFirstLineHeader: boolean;
     nameIndex: number;
     teamIndex: number;
     pointsIndex: number;
     salaryIndex: number;
+    floorIndex?: number;
+    ceilingIndex?: number;
 }
 
 export default class DFSR implements IPlayerInsightRetriever {
     // URLs for DraftKings data
-    private static draftKingsMLBSiteURL = "/data/baseball.draft_kings.csv";
-    private static draftKingsNBASiteURL = "/data/basketball.draft_kings.csv";
+    private static draftKingsMLBSiteURL = "/data/fantasydata.mlb.csv";
+    private static draftKingsNBASiteURL = "/data/basketball.DFSR1.csv";
     private static draftKingsNFLSiteURL = "/data/football.draft_kings.csv";
-    private static draftKingsNHLSiteURL = "/data/hockey.draft_kings.csv";
+    private static draftKingsNHLSiteURL = "/data/hockey.DFSR.csv";
 
     // URLs for FanDuel data
-    private static fanDuelMLBSiteURL = "/data/baseball.fan_duel.csv";
-    private static fanDuelNBASiteURL = "/data/basketball.fan_duel.csv";
+    private static fanDuelMLBSiteURL = "/data/fantasydata.mlb.csv";
+    private static fanDuelNBASiteURL = "/data/basketball.DFSR1.csv";
     private static fanDuelNFLSiteURL = "/data/football.fan_duel.csv";
-    private static fanDuelNHLSiteURL = "/data/hockey.fan_duel.csv";
+    private static fanDuelNHLSiteURL = "/data/hockey.DFSR.csv";
 
     // URLs for Yahoo data
-    private static yahooMLBSiteURL = "/data/baseball.yahoo.csv";
-    private static yahooNBASiteURL = "/data/basketball.yahoo.csv";
+    private static yahooMLBSiteURL = "/data/fantasydata.mlb.csv";
+    private static yahooNBASiteURL = "/data/basketball.DFSR1.csv";
     private static yahooNFLSiteURL = "/data/football.yahoo.csv";
-    private static yahooNHLSiteURL = "/data/hockey.yahoo.csv";
+    private static yahooNHLSiteURL = "/data/hockey.DFSR.csv";
 
-    // Indices for the sports
-    private static mlbIndices: IIndexMapping = {
+    // CSV options for MLB
+    private static draftKingsMLBCSVOptions: ICSVOptions = {
+        isFirstLineHeader: true,
         nameIndex: 1,
-        teamIndex: 10,
-        pointsIndex: 2,
-        salaryIndex: 3
+        teamIndex: 7,
+        pointsIndex: 12,
+        salaryIndex: 11,
+        floorIndex: 40,
+        ceilingIndex: 41
     };
-    private static nbaIndices: IIndexMapping = {
+    private static fanDuelMLBCSVOptions: ICSVOptions = {
+        isFirstLineHeader: true,
+        nameIndex: 1,
+        teamIndex: 7,
+        pointsIndex: 2,
+        salaryIndex: 3,
+        floorIndex: 38,
+        ceilingIndex: 39
+    };
+    private static yahooMLBCSVOptions: ICSVOptions = {
+        isFirstLineHeader: true,
+        nameIndex: 1,
+        teamIndex: 7,
+        pointsIndex: 14,
+        salaryIndex: 13
+    };
+
+    // CSV options for NBA
+    private static draftKingsNBACSVOptions: ICSVOptions = {
+        isFirstLineHeader: true,
+        nameIndex: 1,
+        teamIndex: 7,
+        pointsIndex: 28,
+        salaryIndex: 27
+    };
+    private static fanDuelNBACSVOptions: ICSVOptions = {
+        isFirstLineHeader: true,
         nameIndex: 1,
         teamIndex: 7,
         pointsIndex: 2,
         salaryIndex: 3
     };
-    private static nflIndices: IIndexMapping = {
+    private static yahooNBACSVOptions: ICSVOptions = undefined;
+
+    // CSV options for NFL
+    private static draftKingsNFLCSVOptions: ICSVOptions = {
+        isFirstLineHeader: false,
         nameIndex: 1,
         teamIndex: 2,
         pointsIndex: 4,
         salaryIndex: 6
     };
-    private static nhlIndices: IIndexMapping = {
+    private static fanDuelNFLCSVOptions: ICSVOptions = {
+        isFirstLineHeader: false,
         nameIndex: 1,
-        teamIndex: 6,
+        teamIndex: 2,
+        pointsIndex: 4,
+        salaryIndex: 6
+    };
+    private static yahooNFLCSVOptions: ICSVOptions = undefined;
+
+    // CSV options for NHL
+    private static draftKingsNHLCSVOptions: ICSVOptions = {
+        isFirstLineHeader: true,
+        nameIndex: 1,
+        teamIndex: 5,
+        pointsIndex: 9,
+        salaryIndex: 8
+    };
+    private static fanDuelNHLCSVOptions: ICSVOptions = {
+        isFirstLineHeader: true,
+        nameIndex: 1,
+        teamIndex: 5,
         pointsIndex: 2,
         salaryIndex: 4
     };
+    private static yahooNHLCSVOptions: ICSVOptions = undefined;
 
     async playerInsight(contestType: ContestType, sport: Sport): Promise<IPlayer[]> {
         const playerFactory = new PlayerFactory(sport);
@@ -60,77 +115,85 @@ export default class DFSR implements IPlayerInsightRetriever {
             case ContestType.DraftKings:
                 switch (sport) {
                     case Sport.MLB:
-                        return await this.getData(playerFactory, DFSR.draftKingsMLBSiteURL, DFSR.mlbIndices);
+                        return await this.getData(playerFactory, DFSR.draftKingsMLBSiteURL, DFSR.draftKingsMLBCSVOptions);
                     case Sport.NBA:
-                        return await this.getData(playerFactory, DFSR.draftKingsNBASiteURL, DFSR.nbaIndices);
+                        return await this.getData(playerFactory, DFSR.draftKingsNBASiteURL, DFSR.draftKingsNBACSVOptions);
                     case Sport.NFL:
-                        return await this.getData(playerFactory, DFSR.draftKingsNFLSiteURL, DFSR.nflIndices);
+                        return await this.getData(playerFactory, DFSR.draftKingsNFLSiteURL, DFSR.draftKingsNFLCSVOptions);
                     case Sport.NHL:
-                        return await this.getData(playerFactory, DFSR.draftKingsNHLSiteURL, DFSR.nhlIndices);
+                        return await this.getData(playerFactory, DFSR.draftKingsNHLSiteURL, DFSR.draftKingsNHLCSVOptions);
                 }
                 break;
             case ContestType.FanDuel:
                 switch (sport) {
                     case Sport.MLB:
-                        return await this.getData(playerFactory, DFSR.fanDuelMLBSiteURL, DFSR.mlbIndices);
+                        return await this.getData(playerFactory, DFSR.fanDuelMLBSiteURL, DFSR.fanDuelMLBCSVOptions);
                     case Sport.NBA:
-                        return await this.getData(playerFactory, DFSR.fanDuelNBASiteURL, DFSR.nbaIndices);
+                        return await this.getData(playerFactory, DFSR.fanDuelNBASiteURL, DFSR.fanDuelNBACSVOptions);
                     case Sport.NFL:
-                        return await this.getData(playerFactory, DFSR.fanDuelNFLSiteURL, DFSR.nflIndices);
+                        return await this.getData(playerFactory, DFSR.fanDuelNFLSiteURL, DFSR.fanDuelNFLCSVOptions);
                     case Sport.NHL:
-                        return await this.getData(playerFactory, DFSR.fanDuelNHLSiteURL, DFSR.nhlIndices);
+                        return await this.getData(playerFactory, DFSR.fanDuelNHLSiteURL, DFSR.fanDuelNHLCSVOptions);
                 }
                 break;
             case ContestType.Yahoo:
                 switch (sport) {
                     case Sport.MLB:
-                        return await this.getData(playerFactory, DFSR.yahooMLBSiteURL, DFSR.mlbIndices);
+                        return await this.getData(playerFactory, DFSR.yahooMLBSiteURL, DFSR.yahooMLBCSVOptions);
                     case Sport.NBA:
-                        return await this.getData(playerFactory, DFSR.yahooNBASiteURL, DFSR.nbaIndices);
+                        return await this.getData(playerFactory, DFSR.yahooNBASiteURL, DFSR.yahooNBACSVOptions);
                     case Sport.NFL:
-                        return await this.getData(playerFactory, DFSR.yahooNFLSiteURL, DFSR.nflIndices);
+                        return await this.getData(playerFactory, DFSR.yahooNFLSiteURL, DFSR.yahooNFLCSVOptions);
                     case Sport.NHL:
-                        return await this.getData(playerFactory, DFSR.yahooNHLSiteURL, DFSR.nhlIndices);
+                        return await this.getData(playerFactory, DFSR.yahooNHLSiteURL, DFSR.yahooNHLCSVOptions);
                 }
                 break;
         }
         throw new Error("An unknown contest type or sport was specified");
     }
 
-    private async getData(playerFactory: PlayerFactory, siteURL: string, indices: IIndexMapping): Promise<IPlayer[]> {
+    private async getData(playerFactory: PlayerFactory, siteURL: string, csvOptions: ICSVOptions): Promise<IPlayer[]> {
+        if (!csvOptions) {
+            return Promise.resolve<IPlayer[]>(undefined);
+        }
         return utils.sendHttpsRequest({
             hostname: "www.dailyfantasysportsrankings.com",
             path: siteURL,
             method: "GET"
         }).then((dataResp) => {
-            return this.parsePlayersIndices(playerFactory, dataResp.body, indices);
+            return this.parsePlayersIndices(playerFactory, dataResp.body, csvOptions);
         });
     }
 
-    private parsePlayersIndices(playerFactory: PlayerFactory, data: string, indices: IIndexMapping): IPlayer[] {
-        if (!indices) {
-            return undefined;
-        }
-        return this.parsePlayers(playerFactory, data, indices.nameIndex, indices.teamIndex, indices.pointsIndex, indices.salaryIndex);
+    private parsePlayersIndices(playerFactory: PlayerFactory, data: string, csvOptions: ICSVOptions): IPlayer[] {
+        return this.parsePlayers(playerFactory, data, csvOptions);
     }
 
-    parsePlayers(playerFactory: PlayerFactory, data: string, nameIndex: number, teamIndex: number, pointsIndex: number, salaryIndex: number): IPlayer[] {
+    parsePlayers(playerFactory: PlayerFactory, data: string, csvOptions: ICSVOptions): IPlayer[] {
         if (data) {
+            const { isFirstLineHeader, nameIndex, teamIndex, pointsIndex, salaryIndex, floorIndex, ceilingIndex } = csvOptions;
+            let isFirstLine = true;
             return data.split(/\r?\n/).map(line => {
                 if (line) {
-                    const parts = line.split(/,/);
-                    if (parts && nameIndex < parts.length && teamIndex < parts.length && pointsIndex < parts.length && salaryIndex < parts.length) {
-                        const name = parts[nameIndex];
-                        const team = parts[teamIndex];
-                        const salary = utils.coerceInt(parts[salaryIndex]);
-                        const player = playerFactory.createPlayer(name, team, salary);
-                        player.stats = [
-                            {
-                                source: "DailyFantasySportsRankings",
-                                projectedPoints: utils.coerceFloat(parts[pointsIndex])
-                            }
-                        ];
-                        return player;
+                    const isCurrentFirstLine = isFirstLine;
+                    isFirstLine = false;
+                    if (!isCurrentFirstLine || !isFirstLineHeader) {
+                        const parts = line.split(/,/);
+                        if (parts && nameIndex < parts.length && teamIndex < parts.length && pointsIndex < parts.length && salaryIndex < parts.length) {
+                            const name = parts[nameIndex];
+                            const team = parts[teamIndex];
+                            const salary = utils.coerceInt(parts[salaryIndex]);
+                            const player = playerFactory.createPlayer(name, team, salary);
+                            player.stats = [
+                                {
+                                    source: "DailyFantasySportsRankings",
+                                    projectedPoints: utils.coerceFloat(parts[pointsIndex]),
+                                    projectedFloor: (floorIndex < parts.length) ? utils.coerceFloat(parts[floorIndex]) : undefined,
+                                    projectedCeiling: (ceilingIndex < parts.length) ? utils.coerceFloat(parts[ceilingIndex]) : undefined
+                                }
+                            ];
+                            return player;
+                        }
                     }
                 }
                 return undefined;
